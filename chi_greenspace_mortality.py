42582eecbaf9c2d8dad7ec486a9967352604c37b
Sarah Gill <gills@uchicago.edu>
2020-01-17 09:26:44 -0600

    plots and udate code
    Plot still works
    Weird: the regns yelid different results! less significant too

--- chi_greenspace_mortality.py
+++ chi_greenspace_mortality.py
@@ -1,17 +1,27 @@
+#I should propbably start over with the data now avail on health-atlas!
+#they have rates already!
+#cardiovascular, veg index, per/capita income
+#plus limited access to food
+#or grocery stores per community area
+
+
+
 
 import pandas as pd
 import pandas_datareader.data as web
 from pandas_datareader import wb
 import requests
 import os
-
+import statsmodels.api as sm
+import statsmodels.formula.api as smf 
+import matplotlib.pyplot as plt
 
 
 PATH= '/Users/Sarah/Documents/GitHub/chi_greenspace_mortality'
 os.chdir(os.path.expanduser(PATH)) #set working directory 
 URL = 'https://citytech-health-atlas-data-prod.s3.amazonaws.com/uploads/uploader/path/557/Total_population.xlsx'
-url = 'https://data.cityofchicago.org/api/views/kn9c-c2s2/rows.csv?accessType=DOWNLOAD' 
-filemane = 'Chicago_SES.csv'
+#url = 'https://data.cityofchicago.org/api/views/kn9c-c2s2/rows.csv?accessType=DOWNLOAD' 
+#filemane = 'Chicago_SES.csv'
 
 URLS = [('https://data.cityofchicago.org/api/views/kn9c-c2s2/rows.csv?accessType=DOWNLOAD', 'Chicago_SES.csv'), 
         ('https://citytech-health-atlas-data-prod.s3.amazonaws.com/uploads/uploader/path/721/Green_Index__Land_Cover___Ave_Annual__v2.xlsx', 'Chicago_Green.xls'),
@@ -236,7 +246,20 @@ def re_name(df):
     return df
 
 use_df = re_name(use_df)
-use_df["Ave_Annual_Number"]
+#use_df["Ave_Annual_Number"]
+
+#Limitations in variation in greenspace (over half of the dataset is at 0 avg ann perc green!)
+#look at a restricted sample, exclude 0 values.
+def restricted_df(use_df):
+    some_green = use_df['Ave_Annual_Perc_Green'] != 0
+    some_green_df = use_df[some_green]
+    some_green_df.reset_index(drop = True, inplace = True) #reset the index, needed for plotting later
+    return some_green_df
+
+#cite https://chrisalbon.com/python/data_wrangling/pandas_selecting_rows_on_conditions/  
+#cite https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.reset_index.html
+
+some_green_df = restricted_df(use_df)
 
 def covt_check(df, y_string, x_string):
     print(y_string + ' on ' + x_string)
@@ -250,12 +273,80 @@ covariate_check_list = [('Ave_Annual_Perc_Green', 'Hardship_Index'),
                         ('Ave_Annual_Perc_Green', 'Percent_Households_Below_Poverty')]
 for i, j in covariate_check_list:
     covt_check(use_df, i, j)   
-for i, j in covariate_check_list:
-    covt_check(some_green_df, i, j) 
 
+
+def ols(use_df, y):
+    print('Dependent Variable: ' + y) #for output display
+    m = smf.ols(y + '~ Ave_Annual_Perc_Green + Hardship_Index + Count_Of_Health_Crs' , data = use_df)
+    result = m.fit()
+    print(result.summary()) #show results in output
+    print( ) #for output display readability
+    #prepare results to save to png
+    plt.rc('figure',figsize=(9, 5.5))
+    plt.text(0.01, 0.05, str(result.summary()), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.axis('off')
+    plt.tight_layout()
+    plt.title('Death by '+ y +' on Green-Space, Controling for Area SES and Health Centers')
 
 
 to_plot = [('Suicide','Average Anual Deaths by Suicide'), 
                 ('Diabetes_Related','Average Anual Diabetes Related Deaths'),
                 ('Coronary_Heart_Disease','Average Anual Deaths from Coronary Heart Disease'),
                 ('Stroke','Average Anual Deaths by Stroke')]
+
+for y, ylab in to_plot:
+    ols(some_green_df, y)
+    plt.savefig(y + '_reg_output.png')
+    plt.close()
+
+
+#plot: x:greenspace, y:avg anual deaths, size:per capita income in $1000
+def death_green_SES_plot(df, y, ylabel):
+    x = 'Ave_Annual_Perc_Green'
+    z = df['Community_Area_Name']
+    a_list = df['Per_Capita_Income']/1000
+    ax = df.plot(kind='scatter', x=x, y= y , s = a_list, figsize= (10,8))
+    #cite https://github.com/pandas-dev/pandas/issues/16827
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel('Percent of Area that is Green')
+    #lable point with Community_Area_Name, only if enough green or max death (for viewability) 
+    temp_list = []
+    for i, txt in enumerate(z): 
+        if df[x][i] >= 2:
+            ax.annotate(txt, (df[x][i], df[y][i]), horizontalalignment='center', verticalalignment='bottom')
+        elif df[y][i] == df[y].max():
+            temp_list.append(i)
+            temp_list.append(txt)
+            if len(temp_list) >=4:
+                ax.annotate(temp_list[1], (df[x][temp_list[0]], df[y][temp_list[0]]), 
+                            horizontalalignment='left', verticalalignment='bottom')
+                ax.annotate(temp_list[3], (df[x][temp_list[2]], df[y][temp_list[2]]), 
+                            horizontalalignment='left', verticalalignment='top')
+            else:
+                ax.annotate(temp_list[1], (df[x][temp_list[0]], df[y][temp_list[0]]), 
+                            horizontalalignment='left', verticalalignment='bottom')
+        else:
+            pass   
+    #cite: https://stackoverflow.com/questions/14432557/matplotlib-scatter-plot-with-different-text-at-each-data-point
+    #https://matplotlib.org/3.1.0/gallery/text_labels_and_annotations/annotation_demo.html
+    #remove spines
+    ax.spines['right'].set_visible(False) 
+    ax.spines['top'].set_visible(False)
+    # Make a legend for per-capita income
+    for a_list in [10, 20, 30]:
+        plt.scatter([], [], c='k', alpha=0.3, s=a_list,
+                    label=str(a_list) + 'k')
+    plt.legend(scatterpoints=1, frameon=False, labelspacing=1, title='Per-Capita Income')
+    #cite https://jakevdp.github.io/PythonDataScienceHandbook/04.06-customizing-legends.html
+    #plt.savefig('SES_Green_and_Deaths_by_' +y)
+    #plt.close()
+    #plt.show()
+
+
+
+for col, ylab in to_plot:
+    death_green_SES_plot(some_green_df, col, ylab)
+    plt.title('There is a Negative Relationship between Greenspace and Mortality Rate')
+    plt.suptitle('Plot of Community Areas with Non-Zero Greenspace')
+    plt.savefig('SES_Green_and_Deaths_by_' +col)
+    plt.close()
